93019f6834456eeceec9ba0a5f6c3636d9d36ff7
saebr <bensaebr@gmail.com>
2024-04-27 15:53:12 -0500

    Connected frontend to backend and added needed dependancies

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+//Sends message from server to frontend
+app.get('/test', (req, res) => {
+    console.log('Recieved info from frontend')
+    res.json({ message: "Hello from server!" });
+});
 
 //Start the server
 app.listen(port, () => {
     console.log('Backend server is now running on port 3001')
 });
+
 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  function queryBackend() {
+    fetch("http://localhost:3001/test")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message)).then(console.log(message))
+  }
+
   return (
-    <div className="App">
-    </div>
+    <button onClick={queryBackend} className="border-solid border-2 border-sky-500">Save Changes</button>
+
   );
 }
 
